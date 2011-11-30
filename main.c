8b0cd522d34ca1e0591c0b7c647770afa0e70dbe
Sergey Mironov <ierton@gmail.com>
2011-11-30 16:07:09 +0400

    main: demo printing

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
+#include <common.h>
+
 // 1 or 2 seconds on 300 MHz
 #define DELAY (30*100000)
 
 void main( void )
 {
+	console_init();
+
+	puts("Hello, Arm\n");
+	printf("Hello, Arm, %d times\n", 33);
+
 	volatile int i;
 	while(1) {
 		*((long int*)0x2002a3fc)=0x40;
@@ -10,4 +17,5 @@ void main( void )
 		*((long int*)0x2002a3fc)=0x00;
 		for(i=0; i<DELAY; i++);
 	}
+	hang();
 }
