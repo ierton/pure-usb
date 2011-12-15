1438fae9de8d4c7b6f28dfc9359c267fd5de9247
Sergey Mironov <ierton@gmail.com>
2011-12-15 14:21:13 +0400

    time: split definitions of delay functions
    model's delays should do nothing as model is slow enough anyway

---
+++ time_model.c
@@ -0,0 +1,12 @@
+#include <common.h>
+
+void mdelay(unsigned long msec)
+{
+	/* model is slow enough to fo nothing here */
+}
+
+void udelay(unsigned long usec)
+{
+	/* model is slow enough to do nothing here */
+}
+
